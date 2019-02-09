3c40134a4688a4c9b23ff9b4eeba65ec124dc0f3
arp <irphathil@gmail.com>
2019-02-10 00:34:11 +0100

    longoptions containing - as a separator between words should work now.

--- manparser.lua
+++ manparser.lua
@@ -18,7 +18,7 @@ tab.make_option_table = function(manpage)
 	local line = manpage:read("l")
 	local table = {}
 	while (line ~= nil) do
-		s,l = line:match('^%s+%-(%w+),.+%-%-(%w+)')
+		s,l = line:match('^%s+%-(%w+),.+%-%-([%w-]+)')
 		if (s ~= nil and l ~= nil) then
 			table[s]=l
 		end
